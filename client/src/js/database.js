71697b93e032a28879d86d54902395510407c0aa
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2023-01-24 20:05:48 -0600

    added assets and updated src

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => { 
+  console.log('PUT to the database');
+  //connect to DB and version we want to use
+  const textDb = await openDB('text',1);
+  //make new transaction , specify the db we posting to and the data privilege of "readwrite"
+  const tx = textDb.transaction('text','readwrite');
+  //open object store
+  const store = tx.objectStore('text');
+  //use .put() method to pass in content
+  const request = store.put({ text: content });
+  //confirm teh data was added
+  const result = await request; console.log('🚀 - data saved to the database', result)
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.log('GET all from the database');
+  //connect to DB and version we want to use
+  const todosDb = await openDB('text', 1);
+  // make new transaction , specify the db we posting to and the data privilege of "readonly"
+  const tx = todosDb.transaction('text', 'readonly');
+  //open the object store
+  const store = tx.objectStore('text');
+  //use .getAll() method to grab all the content in the DB
+  const request = store.getAll();
+  //confirm the data was fetched
+  const result = await request;
+  console.log('result.value', result);
+  // return result.value;
+}
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Define Text Editor'
+      }),
+      //service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js'
+    }),
+    //manifest.json
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Define Text Editor',
+      short_name: 'D.T.E',
+      description: 'Keep track of important tasks!',
+      background_color: '#0F111A',
+      theme_color: '#0F111A',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    })
     ],
 
     module: {
+      // TODO: Add CSS loaders and babel to webpack.
+      //add css loader
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        //add babel
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
